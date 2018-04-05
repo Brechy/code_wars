5981a42cb148f3455c2a176a206f4b28e660492d
Corina Andariese <candariese@gmail.com>
2018-04-05 10:57:49 -0700

    Updated with new solutions to Categorize New Member & Vowel Count problems in Codewars.

--- code_wars.js
+++ code_wars.js
@@ -56,3 +56,17 @@ function openOrSenior(data) {
   }
   return memberCategories;
 }
+
+function getCount(str) {
+  var vowelsCount = 0;
+  let vowels = 'aeiou';
+
+  for (let i = 0; i < str.length; i++) {
+    if (vowels.indexOf(str[i]) >= 0) {
+      vowelsCount = vowelsCount+1;
+    }
+
+  }
+
+  return vowelsCount;
+}
